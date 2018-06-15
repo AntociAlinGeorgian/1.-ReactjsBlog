db455515b1b79a301bc1f04860305c34f639c0b6
Alin Georgian, Antoci <brinza.alin.georgian@ro.ibm.com>
2018-06-15 15:32:56 +0300

    Finished

---
+++ simple-website/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "./Navbar.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md">
+        <div className="container">
+          <a href="#" className="navbar-brand">
+            Website
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">
+                  Home <span className="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
---
+++ simple-website/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer";
+import Jumbotron from "../components/Jumbotron";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Jumbotron title="Welcome" subtitle="Put something witty here!" />
+        <div className="container">
+          <h2>Welcome</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            architecto reiciendis numquam. Voluptate quos consequatur nobis
+            veritatis exercitationem vel magnam excepturi repudiandae dicta.
+            Sint molestiae similique exercitationem ipsa, impedit sapiente
+            doloribus quasi sequi, libero a illo consequuntur voluptate atque
+            corporis facere vero ut nemo deserunt rem! Tempora quia sit earum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            reprehenderit laudantium commodi ut a praesentium fugiat quisquam
+            hic iste voluptates!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            reprehenderit laudantium commodi ut a praesentium fugiat quisquam
+            hic iste voluptates!
+          </p>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Home;
